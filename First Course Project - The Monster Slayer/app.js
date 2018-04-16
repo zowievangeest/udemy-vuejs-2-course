8ccf9dbfc307b0e92251930aed296ea9a348209b
zowievangeest <zowie@zowievangeest.nl>
2018-04-16 19:32:02 +0200

    chore(vue-js-2-the-complete-guide): Section 4 - 52

--- First Course Project - The Monster Slayer/app.js
+++ First Course Project - The Monster Slayer/app.js
@@ -12,26 +12,19 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack: function(){ 
-            var max = 10;
-            var min = 3;
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.monsterHealth -= damage;
+            this.monsterHealth -= this.calculateDamage(3, 10);;
 
-            if(this.monsterHealth <= 0) {
-                alert('You won!');
-                this.gameIsRunning = false;
+            if(this.checkWin()) {
                 return;
             }
 
-            max = 12;
-            min = 5;
-            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.playerHealth -= damage
+            this.playerHealth -= this.calculateDamage(5, 12);
 
             if(this.playerHealth <= 0) {
                 alert('You Lost!');
                 this.gameIsRunning = false;
             }
+            this.checkWin();
         },
         specialAttack: function() {
 
@@ -41,6 +34,27 @@ new Vue({
         },
         giveUp: function(){
 
+        },
+        calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
+            if(this.monsterHealth <= 0) {
+                if(confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if(this.playerHealth <= 0 ) {
+                if(confirm('You Lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 });
