2ee64c9eecc87c52f65d2e3b362d4e189bb36b15
zowievangeest <zowie@zowievangeest.nl>
2018-04-16 20:01:08 +0200

    chore(vue-js-2-the-complete-guide): Section 4 - 58

--- First Course Project - The Monster Slayer/app.js
+++ First Course Project - The Monster Slayer/app.js
@@ -11,6 +11,7 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function(){ 
             var damage = this.calculateDamage(3, 10);
@@ -26,7 +27,12 @@ new Vue({
             this.monsterAttacks();
         },
         specialAttack: function() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -38,6 +44,10 @@ new Vue({
             } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
             this.monsterAttacks();
         },
         giveUp: function(){
