b1e659ce871684d2a021c858cee0da13c5cc4e6d
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 14:13:31 +0100

    chore(vue-js-2-the-complete-guide): Section 10 - 134

---
+++ Second Course Project - Wonderful Quotes/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {
+                quotes: [
+                    'Just a Quote to see something'
+                ],
+                maxQuotes: 10,
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
