a802f40fce0dc5f5f5753bfe40d6fccf4e5edd69
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 13:30:05 +0100

    chore(vue-js-2-the-complete-guide): Section 9 - 123

--- Advanced Component Usage/src/components/Quote.vue
+++ Advanced Component Usage/src/components/Quote.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-<slot></slot>
+        <div class="title">
+            <slot name="title"></slot>
+        </div>
+        <hr>
+        <div>
+            <slot name="content"></slot>
+        </div>
     </div>
 </template>
 
@@ -19,5 +25,8 @@ export default {
     }
     h2 {
         color: red;
+    }
+    .title {
+        font-style: italic;
     }
 </style>
