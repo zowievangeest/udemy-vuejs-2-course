b8405d0ce4ebcec3784025efcc70516b58f411f6
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 12:14:08 +0100

    chore(vue-js-2-the-complete-guide): Section 8 - 108

--- Communication Problems/src/components/UserDetail.vue
+++ Communication Problems/src/components/UserDetail.vue
@@ -8,7 +8,11 @@
 
 <script>
 export default {
-    props: ['myName'],
+    props: {
+        myName: {
+            type: String,
+        }
+    },
     methods: {
         switchName() {
             return this.myName.split("").reverse().join("");
