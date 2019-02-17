1342cbcbd717abce78df5ed5b39c49e6098e5136
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 11:49:53 +0100

    chore(vue-js-2-the-complete-guide): Section 7 - 97

--- Opdracht 6 - Time to Practice/src/App.vue
+++ Opdracht 6 - Time to Practice/src/App.vue
@@ -12,10 +12,10 @@
 </template>
 
 <script>
-    import Header from './Header.vue';
-    import Footer from './Footer.vue';
-    import Servers from './Servers.vue';
-    import ServerDetails from './ServerDetails.vue';
+    import Header from './components/Shared/Header.vue';
+    import Footer from './components/Shared/Footer.vue';
+    import Servers from './components/Server/Servers.vue';
+    import ServerDetails from './components/Server/ServerDetails.vue';
 
     export default {
         components: {
