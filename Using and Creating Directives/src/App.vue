b11f8edd9325d29a599c9614a5973e807b034824
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 16:29:13 +0100

    chore(vue-js-2-the-complete-guide): Section 12 - 165

--- Using and Creating Directives/src/App.vue
+++ Using and Creating Directives/src/App.vue
@@ -12,13 +12,33 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
+                <p v-local-highlight:background.delayed="'red'">Color this</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {}
+    export default {
+        directives: {
+            'local-highlight': {
+                bind(el, binding, vnode) {
+                    let delay = 0;
+
+                    if(binding.modifiers['delayed']) {
+                        delay = 3000;
+                    }
+                    setTimeout(() => {
+                        if(binding.arg === 'background') {
+                            el.style.backgroundColor = binding.value;
+                        } else {
+                            el.style.color = binding.value;
+                        }
+                    }, delay);
+                }
+            }
+        }
+    }
 </script>
 
 <style>
