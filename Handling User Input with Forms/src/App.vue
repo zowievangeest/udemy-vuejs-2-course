94b03217ff4c58aeb488f263dda2930449061d33
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 15:37:34 +0100

    chore(vue-js-2-the-complete-guide): Section 11 - 151

--- Handling User Input with Forms/src/App.vue
+++ Handling User Input with Forms/src/App.vue
@@ -87,8 +87,9 @@
                     <label for="priority">Priority</label>
                     <select
                             id="priority"
-                            class="form-control">
-                        <option></option>
+                            class="form-control"
+                            v-model="selectedPriority">
+                        <option v-for="priority in priorities">{{ priority }}</option>
                     </select>
                 </div>
             </div>
@@ -118,7 +119,7 @@
                             <li v-for="item in sendMail">{{ item }}</li>
                         </ul>
                         <p>Gender: {{ gender }}</p>
-                        <p>Priority:</p>
+                        <p>Priority: {{ selectedPriority }}</p>
                         <p>Switched:</p>
                     </div>
                 </div>
@@ -138,7 +139,9 @@
                 },
                 message: 'NEw Text',
                 sendMail: [],
-                gender: 'Male'
+                gender: 'Male',
+                selectedPriority: 'High',
+                priorities: ['High', 'Medium', 'Low']
             }
         }
     }
