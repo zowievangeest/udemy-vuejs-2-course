02cd3c69d22e3679dcde118ee13aa8250ac8e0d9
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 16:41:53 +0100

    chore(vue-js-2-the-complete-guide): Section 12 - 167

--- Using and Creating Directives/src/App.vue
+++ Using and Creating Directives/src/App.vue
@@ -12,7 +12,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: 500}">Color this</p>
             </div>
         </div>
     </div>
@@ -29,8 +29,8 @@
                         delay = 3000;
                     }
                     if(binding.modifiers['blink']) {
-                        let mainColor = binding.value;
-                        let secondColor = 'blue';
+                        let mainColor = binding.value.mainColor;
+                        let secondColor = binding.value.secondColor;
                         let currentColor = mainColor;
 
                         setTimeout(() => {
@@ -41,14 +41,14 @@
                                 } else {
                                     el.style.color = currentColor;
                                 }
-                            },1000);
+                            },binding.value.delay);
                         }, delay);
                     } else {
                         setTimeout(() => {
                             if(binding.arg === 'background') {
-                                el.style.backgroundColor = binding.value;
+                                el.style.backgroundColor = binding.value.mainColor;
                             } else {
-                                el.style.color = binding.value;
+                                el.style.color = binding.value.mainColor;
                             }
                         }, delay);
                     }
