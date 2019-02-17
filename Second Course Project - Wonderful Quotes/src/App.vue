90a60c77ebf34cac2cd62fb63b740979660cd93a
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 14:37:06 +0100

    chore(vue-js-2-the-complete-guide): Section 10 - 140

--- Second Course Project - Wonderful Quotes/src/App.vue
+++ Second Course Project - Wonderful Quotes/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
             <div class="col-sm-12 text-center">
                 <div class="alert alert-info">Info: Click on a quote to delete this!</div>
@@ -28,8 +28,11 @@
             appNewQuote: NewQuote,
         },
         methods: {
-            newQuote(quote){
+            newQuote(quote) {
                 this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
             }
         }
     }
