1114caea2709aedb218499132671e8f7f3038d6a
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 15:47:50 +0100

    chore(vue-js-2-the-complete-guide): Section 11 - 153

--- Handling User Input with Forms/src/App.vue
+++ Handling User Input with Forms/src/App.vue
@@ -97,6 +97,11 @@
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
                             class="btn btn-primary">Submit!
                     </button>
@@ -121,7 +126,7 @@
                         </ul>
                         <p>Gender: {{ gender }}</p>
                         <p>Priority: {{ selectedPriority }}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{ dataSwitch }}</p>
                     </div>
                 </div>
             </div>
@@ -130,6 +135,8 @@
 </template>
 
 <script>
+    import Switch from "./Switch.vue";
+
     export default {
         data() {
             return {
@@ -142,8 +149,12 @@
                 sendMail: [],
                 gender: 'Male',
                 selectedPriority: 'High',
-                priorities: ['High', 'Medium', 'Low']
+                priorities: ['High', 'Medium', 'Low'],
+                dataSwitch: true
             }
+        },
+        components: {
+            appSwitch: Switch
         }
     }
 </script>
