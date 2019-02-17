42e127b520d3a944d2845a9e7bc035abc44f4583
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 14:27:43 +0100

    chore(vue-js-2-the-complete-guide): Section 10 - 137

---
+++ Second Course Project - Wonderful Quotes/src/components/NewQuote.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row">
+        <form action="">
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <label>Quote</label>
+                <textarea class="form-control" rows="3" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+    </div>
+</template>
+<script>
+    export default {
+        data: function () {
+            return {
+                quote: ''
+            }
+        },
+        methods: {
+          createNew() {
+
+          }
+        }
+    }
+</script>
+<style>
+
+</style>
