e0a60efd3479a971b188e837bf1c60ed0046f0a2
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 13:24:46 +0100

    chore(vue-js-2-the-complete-guide): Section 9 - 120

--- Advanced Component Usage/src/App.vue
+++ Advanced Component Usage/src/App.vue
@@ -2,9 +2,10 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote quote="A Wonderful Quote!"></app-quote>
-                <h2>Quote</h2>
-                <p>A wonderful Quote</p>
+                <app-quote quote="A Wonderful Quote!">
+                    <h2>Quote</h2>
+                    <p>A wonderful Quote</p>
+                </app-quote>
             </div>
         </div>
     </div>
