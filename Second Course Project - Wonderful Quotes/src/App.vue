7fde1d1d21c17bce6a29467dc1d22521ae8047db
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 14:52:18 +0100

    chore(vue-js-2-the-complete-guide): Section 10 - 142

--- Second Course Project - Wonderful Quotes/src/App.vue
+++ Second Course Project - Wonderful Quotes/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-        <app-header></app-header>
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -32,7 +32,12 @@
         },
         methods: {
             newQuote(quote) {
-                this.quotes.push(quote);
+                if(this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete quotes first');
+                }
+                if(quote) {
+                    this.quotes.push(quote);
+                }
             },
             deleteQuote(index) {
                 this.quotes.splice(index, 1);
