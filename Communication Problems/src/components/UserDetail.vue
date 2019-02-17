f380d4aeef051ec59a0ae5973e612e1674a28656
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 12:04:48 +0100

    chore(vue-js-2-the-complete-guide): Section 8 - 104

---
+++ Communication Problems/src/components/UserDetail.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{ name }}</p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
