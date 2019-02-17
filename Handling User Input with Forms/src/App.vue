eca10966f48e4e18e225236897548bbce54a80a5
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 15:41:20 +0100

    chore(vue-js-2-the-complete-guide): Section 11 - 152

--- Handling User Input with Forms/src/App.vue
+++ Handling User Input with Forms/src/App.vue
@@ -11,7 +11,8 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model="userData.email">
+                                :value="userData.email"
+                                @input="userData.email = $event.target.value">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
