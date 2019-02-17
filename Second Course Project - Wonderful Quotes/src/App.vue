0daa1736c29469b220afc2722ddd559f06a21865
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 14:44:17 +0100

    chore(vue-js-2-the-complete-guide): Section 10 - 141

--- Second Course Project - Wonderful Quotes/src/App.vue
+++ Second Course Project - Wonderful Quotes/src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -13,6 +14,7 @@
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
     import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
 
     export default {
         data: function () {
@@ -26,6 +28,7 @@
         components: {
             appQuoteGrid: QuoteGrid,
             appNewQuote: NewQuote,
+            appHeader: Header
         },
         methods: {
             newQuote(quote) {
