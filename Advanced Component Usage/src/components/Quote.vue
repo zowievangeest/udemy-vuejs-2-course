fe4759d491ac13ad51a24ee0a07558e852f176b7
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 13:20:22 +0100

    chore(vue-js-2-the-complete-guide): Section 9 - 118

---
+++ Advanced Component Usage/src/components/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>
+            A Wonderful Quote!
+        </p>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+</style>
