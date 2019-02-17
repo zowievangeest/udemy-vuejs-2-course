71769cc6df3f38d64c51eb6d257f0dfaedf2d096
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-17 13:40:06 +0100

    chore(vue-js-2-the-complete-guide): Section 9 - 126

--- Advanced Component Usage/src/App.vue
+++ Advanced Component Usage/src/App.vue
@@ -2,11 +2,19 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-                <app-quote>
-                    <slot name="title"></slot>
-                    <h2 slot="title">{{ quoteTitle }}</h2>
-                    <p>A wonderful Quote</p>
-                </app-quote>
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <component :is="selectedComponent">
+                    <p>Default Content</p>
+                </component>
+                <!--<app-quote>-->
+                    <!--<slot name="title"></slot>-->
+                    <!--<h2 slot="title">{{ quoteTitle }}</h2>-->
+                    <!--<p>A wonderful Quote</p>-->
+                <!--</app-quote>-->
             </div>
         </div>
     </div>
@@ -14,15 +22,20 @@
 
 <script>
     import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
 
     export default {
         data: function() {
             return {
-                quoteTitle: 'The Quote'
+                quoteTitle: 'The Quote',
+                selectedComponent: 'appQuote'
             }
         },
         components: {
             appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
         }
     }
 </script>
