25554cdd8bb27ab497a5f68afff553605f1d37fa
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-19 21:44:29 +0100

    chore(vue-js-2-the-complete-guide): Section 13 - 175

--- Improving your App with Filters and Mixins/src/App.vue
+++ Improving your App with Filters and Mixins/src/App.vue
@@ -18,26 +18,19 @@
 
 <script>
     import List from './List.vue';
+    import { fruitMixin} from './fruitMixin.js';
 
     export default {
+        mixins: [fruitMixin],
         data() {
             return {
                 text: 'Hello there',
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
             }
         },
         filters: {
             toUppercase(value) {
                 return value.toUpperCase();
             }
-        },
-        computed: {
-          filteredFruits() {
-              return this.fruits.filter((element) => {
-                  return element.match(this.filterText);
-              })
-          }
         },
         components: {
             appList: List
--- Improving your App with Filters and Mixins/src/List.vue
+++ Improving your App with Filters and Mixins/src/List.vue
@@ -9,20 +9,10 @@
 </template>
 
 <script>
+    import { fruitMixin } from './fruitMixin.js';
+
     export default {
-        data() {
-            return {
-                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
-                filterText: ''
-            }
-        },
-        computed: {
-            filteredFruits() {
-                return this.fruits.filter((element) => {
-                    return element.match(this.filterText);
-                })
-            }
-        }
+        mixins: [fruitMixin]
     }
 </script>
 
