83c1a1868f753b04c71bfc26a1da16d94efd65bb
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-19 23:20:16 +0100

    chore(vue-js-2-the-complete-guide): Section 14 - 191

--- Adding Animations and Transitions/src/App.vue
+++ Adding Animations and Transitions/src/App.vue
@@ -9,10 +9,13 @@
                 <transition name="fade">
                     <div class="alert alert-info" v-show="show">This is some info</div>
                 </transition>
-                <transition name="slide" type="animation">
+                <transition name="slide" type="animation" appear>
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
-                <transition name="fade" appear>
+                <transition
+                        enter-active-class="animated bounce"
+                        leave-active-class="animated shake"
+                >
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
             </div>
