f0844ff2e120ef4115e1320d1a54f569e781008f
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-19 23:03:45 +0100

    chore(vue-js-2-the-complete-guide): Section 14 - 186

--- Adding Animations and Transitions/src/App.vue
+++ Adding Animations and Transitions/src/App.vue
@@ -26,15 +26,16 @@
 
 <style>
     .fade-enter {
-
+        opacity: 0;
     }
     .fade-enter-active {
-
+        transition: opacity 1s;
     }
     .fade-leave {
-
+        /*opacity: 1;*/
     }
     .fade-leave-active {
-
+        transition: opacity 1s;
+        opacity: 0;
     }
 </style>
