ec7969993058005fed2d779f90fc069799739ff3
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-19 21:26:22 +0100

    chore(vue-js-2-the-complete-guide): Section 13 - 172

--- Improving your App with Filters and Mixins/src/App.vue
+++ Improving your App with Filters and Mixins/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                <p>{{ text | toUppercase }}</p>
+                <p>{{ text | toUppercase | to-lowercase }}</p>
             </div>
         </div>
     </div>
@@ -13,7 +13,7 @@
     export default {
         data() {
             return {
-                text: 'hello there'
+                text: 'Hello there'
             }
         },
         filters: {
