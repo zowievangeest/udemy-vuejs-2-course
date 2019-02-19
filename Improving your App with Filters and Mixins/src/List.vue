4f35e5d6f09e1fe82ffdb3722d1031add34ffd71
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-19 21:40:37 +0100

    chore(vue-js-2-the-complete-guide): Section 13 - 174

---
+++ Improving your App with Filters and Mixins/src/List.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText: ''
+            }
+        },
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter((element) => {
+                    return element.match(this.filterText);
+                })
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
