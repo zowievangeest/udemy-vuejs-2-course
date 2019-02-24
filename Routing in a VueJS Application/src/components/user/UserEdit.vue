894c3e148c2443d6113ad1197648e34000589bf7
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 18:49:27 +0100

    chore(vue-js-2-the-complete-guide): Section 16 - 248

--- Routing in a VueJS Application/src/components/user/UserEdit.vue
+++ Routing in a VueJS Application/src/components/user/UserEdit.vue
@@ -4,8 +4,30 @@
         <p>Local: {{ $route.query.locale }}</p>
         <p>Local: {{ $route.query.q }}</p>
         <hr>
-        <button class="btn btn-primary">Confirm</button>
+        <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
         <div style="height: 700px"></div>
         <p id="data">Some extra data</p>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                confirmed: false
+            }
+        },
+        beforeRouteLeave(to, from, next) {
+            if (this.confirmed) {
+                next();
+            } else {
+                if(confirm('Are you sure?')) {
+                    next();
+                } else {
+                    next(false);
+                }
+            }
+            next();
+        }
+    }
+</script>
