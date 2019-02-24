b912e8d0de2ffd311070cc9b06ec9e63b9f64f66
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 13:45:18 +0100

    chore(vue-js-2-the-complete-guide): Section 14 - 201

--- Adding Animations and Transitions/src/App.vue
+++ Adding Animations and Transitions/src/App.vue
@@ -53,6 +53,18 @@
                 <transition name="fade" mode="out-in">
                     <component :is="selectedComponent"></component>
                 </transition>
+                <hr>
+                <button class="btn btn-primary" @click="addItem">Add Item</button>
+                <br><br>
+                <ul class="list-group">
+                    <li
+                            class="list-group-item"
+                            v-for="(number, index) in numbers"
+                            @click="removeItem(index)"
+                            style="cursor: pointer"
+                    >{{ number }}
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
@@ -69,7 +81,8 @@
                 load: true,
                 alertAnimation: 'fade',
                 elementWidth: 100,
-                selectedComponent: 'app-success-alert'
+                selectedComponent: 'app-success-alert',
+                numbers: [1, 2, 3, 4, 5]
             }
         },
         methods: {
@@ -118,6 +131,13 @@
             },
             leaveCancelled(el) {
                 console.log('leaveCancelled');
+            },
+            addItem() {
+                const pos = Math.floor(Math.random() * this.numbers.length);
+                this.numbers.splice(pos, 0, this.numbers.length + 1);
+            },
+            removeItem(index) {
+                this.numbers.splice(index, 1);
             }
         },
         components: {
