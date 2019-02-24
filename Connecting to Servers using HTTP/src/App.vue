458d3f91079c2f40622e0c0739971020b3a13909
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 16:01:06 +0100

    chore(vue-js-2-the-complete-guide): Section 15 - 221

--- Connecting to Servers using HTTP/src/App.vue
+++ Connecting to Servers using HTTP/src/App.vue
@@ -13,6 +13,7 @@
                 </div>
                 <button class="btn btn-primary" @click="submit">Submit</button>
                 <hr>
+                <input class="form-control" type="text" v-model="node">
                 <button class="btn btn-primary" @click="fetchData">Get Data</button>
                 <ul class="list-group">
                     <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
@@ -31,34 +32,34 @@
                     email: ''
                 },
                 users: [],
-                resource: {}
+                resource: {},
+                node: 'data'
             }
         },
         methods: {
             submit() {
-                // this.$http.post('data.json', this.user)
-                //     .then(response => {
-                //         console.log(response);
-                //     }, error => {
-                //         console.log(error);
-                //     });
-                this.resource.save({}, this.user);
+                this.resource.saveAlt(this.user);
             },
             fetchData: function () {
-                this.$http.get('data.json').then(response => {
-                    return response.json;
-                })
+                this.resource.getData({node: this.node})
+                    .then(response => {
+                        return response.json;
+                    })
                     .then(data => {
                         const resultArray = [];
                         for (let key in data) {
                             resultArray.push(data[key]);
                         }
                         this.users = resultArray;
-                    })
+                    });
             }
         },
         created() {
-            this.resource = this.$resource('data.json');
+            const customActions = {
+                saveAlt: {method: 'POST', url: 'alternative.json'},
+                getData: {method: 'GET'}
+            };
+            this.resource = this.$resource('{node}.json', {}, customActions);
         }
     }
 </script>
