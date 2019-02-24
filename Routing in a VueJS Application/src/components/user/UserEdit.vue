009ca1407352bf040bc9acdc139f6ad9e044d773
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 18:36:25 +0100

    chore(vue-js-2-the-complete-guide): Section 16 - 246

--- Routing in a VueJS Application/src/components/user/UserEdit.vue
+++ Routing in a VueJS Application/src/components/user/UserEdit.vue
@@ -3,6 +3,8 @@
         <h3>Edit the User</h3>
         <p>Local: {{ $route.query.locale }}</p>
         <p>Local: {{ $route.query.q }}</p>
+        <hr>
+        <button class="btn btn-primary">Confirm</button>
         <div style="height: 700px"></div>
         <p id="data">Some extra data</p>
     </div>
