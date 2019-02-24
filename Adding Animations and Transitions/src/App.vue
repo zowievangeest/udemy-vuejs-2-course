b2eef8179e5eb3bf5fc7225927226910b8ee53ec
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 13:05:50 +0100

    chore(vue-js-2-the-complete-guide): Section 14 - 194

--- Adding Animations and Transitions/src/App.vue
+++ Adding Animations and Transitions/src/App.vue
@@ -23,6 +23,10 @@
                 >
                     <div class="alert alert-info" v-if="show">This is some info</div>
                 </transition>
+                <transition :name="alertAnimation" mode="out-in">
+                    <div class="alert alert-info" v-if="show" key="info">This is some info</div>
+                    <div class="alert alert-warning" v-else key="warning">This is some warning</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -73,7 +77,7 @@
 
     .slide-leave-active {
         animation: slide-out 1s ease-out forwards;
-        transition: opacity 3s;
+        transition: opacity 1s;
         opacity: 0;
     }
 
