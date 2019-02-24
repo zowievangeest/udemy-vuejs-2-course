e408f9467f7c8ad933ef22214fecc4f1dcbfcfde
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 18:02:57 +0100

    chore(vue-js-2-the-complete-guide): Section 16 - 238

--- Routing in a VueJS Application/src/components/Header.vue
+++ Routing in a VueJS Application/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <ul class="nav nav-pills">
-        <router-link to="/" tag="li" active-class="active" exact>Home</router-link>
+        <router-link :to="{ name: 'home' }" tag="li" active-class="active" exact>Home</router-link>
         <router-link to="/user" tag="li" active-class="active">User</router-link>
     </ul>
 </template>
