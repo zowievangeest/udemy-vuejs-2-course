593b1d52fd8f110c5fa6a1b0ab4407949db78183
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 18:05:38 +0100

    chore(vue-js-2-the-complete-guide): Section 16 - 239

--- Routing in a VueJS Application/src/components/user/UserEdit.vue
+++ Routing in a VueJS Application/src/components/user/UserEdit.vue
@@ -1,3 +1,7 @@
 <template>
-    <h3>Edit the User</h3>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Local: {{ $route.query.locale }}</p>
+        <p>Local: {{ $route.query.q }}</p>
+    </div>
 </template>
