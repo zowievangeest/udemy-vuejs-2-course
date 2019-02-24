aec926c10f08679a0848febc494a4aaec39adb60
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 16:53:05 +0100

    chore(vue-js-2-the-complete-guide): Section 16 - 235

--- Routing in a VueJS Application/src/components/Header.vue
+++ Routing in a VueJS Application/src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
     <ul class="nav nav-pills">
         <router-link to="/" tag="li" active-class="active" exact>Home</router-link>
-        <router-link to="/user/1" tag="li" active-class="active">User 1</router-link>
-        <router-link to="/user/2" tag="li" active-class="active">User 2</router-link>
+        <router-link to="/user" tag="li" active-class="active">User</router-link>
     </ul>
 </template>
 
--- Routing in a VueJS Application/src/components/user/User.vue
+++ Routing in a VueJS Application/src/components/user/User.vue
@@ -2,24 +2,14 @@
     <div>
         <h1>The User Page</h1>
         <hr>
-        <p>Loaded ID: {{ id }}</p>
         <button class="btn btn-primary" @click="navigateToHome">Go To Home</button>
+        <hr>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-            return {
-                id: this.$route.params.id
-            }
-        },
-        watch: {
-            '$route'(to, from) {
-                this.id = to.params.id;
-
-            }
-        },
         methods: {
             navigateToHome() {
                 this.$router.push('/')
