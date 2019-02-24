0a8f56e9bf138af3cc660273acb6c5952e765d4a
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-25 00:12:53 +0100

    chore(vue-js-2-the-complete-guide): Section 17 - 271

--- Better State Management with Vuex/src/components/AnotherResult.vue
+++ Better State Management with Vuex/src/components/AnotherResult.vue
@@ -7,12 +7,13 @@
 
 <script>
     import {mapGetters} from 'vuex';
+    import * as types from '../store/types';
 
     export default {
         computed: {
             ...mapGetters({
-                counter: 'doubleCounter',
-                clicks: 'stringCounter'
+                counter: types.DOUBLE_COUNTER,
+                clicks: types.CLICK_COUNTER
             })
         }
     }
