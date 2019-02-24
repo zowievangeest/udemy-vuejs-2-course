761d46479eb087f6c836708a59b5a942122f894e
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 13:52:26 +0100

    chore(vue-js-2-the-complete-guide): Section 14 - 202

--- Adding Animations and Transitions/src/App.vue
+++ Adding Animations and Transitions/src/App.vue
@@ -57,13 +57,16 @@
                 <button class="btn btn-primary" @click="addItem">Add Item</button>
                 <br><br>
                 <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="(number, index) in numbers"
-                            @click="removeItem(index)"
-                            style="cursor: pointer"
-                    >{{ number }}
-                    </li>
+                    <transition-group name="slide">
+                        <li
+                                class="list-group-item"
+                                v-for="(number, index) in numbers"
+                                @click="removeItem(index)"
+                                style="cursor: pointer"
+                                :key="number"
+                        >{{ number }}
+                        </li>
+                    </transition-group>
                 </ul>
             </div>
         </div>
@@ -183,6 +186,11 @@
         animation: slide-out 1s ease-out forwards;
         transition: opacity 1s;
         opacity: 0;
+        position: absolute;
+    }
+
+    .slide-move {
+        transition: transform 1s;
     }
 
     @keyframes slide-in {
