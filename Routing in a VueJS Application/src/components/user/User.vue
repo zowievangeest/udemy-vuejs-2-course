6b3605afcb65eb7a3a0c2a05cd9073a70754ab86
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-24 16:42:53 +0100

    chore(vue-js-2-the-complete-guide): Section 16 - 232

--- Routing in a VueJS Application/src/components/user/User.vue
+++ Routing in a VueJS Application/src/components/user/User.vue
@@ -2,12 +2,18 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <p>Loaded ID: {{ id }}</p>
         <button class="btn btn-primary" @click="navigateToHome">Go To Home</button>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        },
         methods: {
             navigateToHome() {
                 this.$router.push('/')
