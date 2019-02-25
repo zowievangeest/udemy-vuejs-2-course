a15af7164363ae20bf21de8a0763c10ee0cbbbb5
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-25 21:58:39 +0100

    chore(vue-js-2-the-complete-guide): Section 18 - 282

--- Final Project - The Stock Trader/src/components/stocks/Stock.vue
+++ Final Project - The Stock Trader/src/components/stocks/Stock.vue
@@ -3,22 +3,50 @@
         <div class="panel panel-success">
             <div class="panel-heading">
                 <h3 class="panel-title">
-                    Name
-                    <small>(Price: PRICE)</small>
+                    {{ stock.name }}
+                    <small>(Price: {{ stock.price }})</small>
                 </h3>
             </div>
             <div class="panel-body">
                 <div class="pull-left">
-                    <input
-                        type="number"
-                        class="form-control"
-                        placeholder="Quantity">
+                    <input v-model.number="quantity"
+                           type="number"
+                           class="form-control"
+                           placeholder="Quantity">
                 </div>
                 <div class="pull-right">
-                    <button
-                        class="btn btn-success">Buy</button>
+                    <button class="btn btn-success"
+                            @click="buyStock"
+                            :disabled="isDisabled">Buy</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    export default {
+        props: ['stock'],
+        data() {
+            return {
+                quantity: 0
+            }
+        },
+        computed: {
+            isDisabled() {
+                return (this.quantity <= 0 || !Number.isInteger(this.quantity))
+            }
+        },
+        methods: {
+            buyStock() {
+                const order = {
+                    stockId: this.stock.id,
+                    stockPrice: this.stock.price,
+                    quantity: this.quantity
+                };
+                console.log(order);
+                this.quantity = 0;
+            }
+        }
+    }
+</script>
