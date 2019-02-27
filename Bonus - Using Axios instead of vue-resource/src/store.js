8152f653629059dc79f6d5f6686ac7dd3e824a89
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-27 12:21:42 +0100

    chore(vue-js-2-the-complete-guide): Section 23 - 352

--- Bonus - Using Axios instead of vue-resource/src/store.js
+++ Bonus - Using Axios instead of vue-resource/src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from './axios-auth';
+import globalAxios from 'axios';
 
 Vue.use(Vuex)
 
@@ -8,15 +9,19 @@ export default new Vuex.Store({
   state: {
     idToken: null,
     userId: null,
+    user: null,
   },
   mutations: {
     authUser (state, userData) {
       state.idToken = userData.token;
       state.userId = userData.userId;
+    },
+    storeUser(state, user){
+      state.user = user;
     }
   },
   actions: {
-    signup({commit}, authData) {
+    signup({commit, dispatch}, authData) {
       axios.post('/signupNewUser?key=AIzaSyAjEEOPkJFQn82INXn3IU--StmmIOrHIn4', {
         email: authData.email,
         password: authData.password,
@@ -27,7 +32,8 @@ export default new Vuex.Store({
             commit('authUser', {
               token: res.data.idToken,
               userId: res.data.localId
-            })
+            });
+            dispatch('storeUser', authData);
           })
           .catch(error => console.log(error));
     },
@@ -45,9 +51,32 @@ export default new Vuex.Store({
             })
           })
           .catch(error => console.log(error));
+    },
+    storeUser({commit}, userData) {
+      globalAxios.post('/users.json', userData)
+          .then(res => console.log(res))
+          .catch( error => console.log(error))
+    },
+    fetchUser({commit}) {
+      globalAxios.get('/users.json')
+          .then(res => {
+            console.log(res);
+            const data = res.data;
+            const users = [];
+            for (let key in data) {
+              const user = data[key];
+              user.id = key;
+              users.push(user);
+            }
+            console.log(users);
+            commit('storeUser', users[0]);
+          })
+          .catch(error => console.log(error))
     }
   },
   getters: {
-
+    user(state) {
+      return state.user;
+    }
   }
 })
