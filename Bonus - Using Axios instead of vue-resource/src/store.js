a875221d358b3aa8d451e9ab01dd39e98e36f9b9
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-27 14:03:35 +0100

    chore(vue-js-2-the-complete-guide): Section 23 - 353

--- Bonus - Using Axios instead of vue-resource/src/store.js
+++ Bonus - Using Axios instead of vue-resource/src/store.js
@@ -52,13 +52,19 @@ export default new Vuex.Store({
           })
           .catch(error => console.log(error));
     },
-    storeUser({commit}, userData) {
-      globalAxios.post('/users.json', userData)
+    storeUser({commit, state}, userData) {
+      if(!state.idToken) {
+        return
+      }
+      globalAxios.post('/users.json' + '?auth=' + state.idToken, userData)
           .then(res => console.log(res))
           .catch( error => console.log(error))
     },
-    fetchUser({commit}) {
-      globalAxios.get('/users.json')
+    fetchUser({commit, state}) {
+      if(!state.idToken) {
+        return
+      }
+      globalAxios.get('/users.json' + '?auth=' + state.idToken)
           .then(res => {
             console.log(res);
             const data = res.data;
