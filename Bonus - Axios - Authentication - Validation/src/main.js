58f6bd6ff3d39da53fc25d050531964b904f5fa6
Zowie van Geest <zowie@zowievangeest.nl>
2019-02-27 15:17:08 +0100

    chore(vue-js-2-the-complete-guide): Section 24 - 366

--- Bonus - Axios - Authentication - Validation/src/main.js
+++ Bonus - Axios - Authentication - Validation/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios';
+import Vuelidate from 'vuelidate';
 
 import router from './router'
 import store from './store'
+
+Vue.use(Vuelidate);
 
 axios.defaults.baseURL = 'https://vue-update-1e8c3.firebaseio.com';
 // axios.defaults.headers.common['Authorization'] = 'sdsdfdsf';
